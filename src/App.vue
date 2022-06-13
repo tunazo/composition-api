9e41438585c9ffa4e4ea7934d2e1f6293014f892
Tuong Huynh <dusitsmg@gmail.com>
2022-06-13 12:23:51 +0700

    Modify project

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,31 @@
 <template>
-  <section class="container">
-    <user-data v-bind="user" @change-age="cAge"></user-data>
-    <input type="text" placeholder="First Name" v-model="user.firstName" />
-    <input type="text" placeholder="Last Name" ref="lastNameInput" />
-    <button @click="setName">change Last Name</button>
-  </section>
+  <main>
+    <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
+    <projects-list :user="selectedUser"></projects-list>
+  </main>
 </template>
 
 <script>
-import UserData from './components/UserData.vue';
-import { ref, reactive, provide } from 'vue';
+import USER_DATA from './dummy-data.js';
+
+import UserList from './components/users/UserList.vue';
+import ProjectsList from './components/projects/ProjectsList.vue';
+
 export default {
   components: {
-    UserData,
+    UserList,
+    ProjectsList,
   },
-  setup() {
-    const user = reactive({
-      firstName: '',
-      lastName: '',
-    });
-
-    const age = ref(32);
-
-    provide('uAge', age);
-
-    function cAge(value) {
-      age.value = value;
-    }
-
-    const lastNameInput = ref(null);
-
-    function setName() {
-      user.lastName = lastNameInput.value.value;
-    }
-
-    // const fullName = computed(() => {
-    //   return `${user.firstName} ${user.lastName}`;
-    // });
-
-    // watch(fullName, (currVal, oldVal) => {
-    //   if (currVal && currVal !== oldVal) {
-    //     console.log('name changed!');
-    //   }
-    // });
+  data() {
     return {
-      setName,
-      lastNameInput,
-      user,
-      cAge,
+      selectedUser: null,
+      activeUsers: USER_DATA,
     };
+  },
+  methods: {
+    selectUser(uid) {
+      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
+    },
   },
 };
 </script>
@@ -57,21 +34,34 @@ export default {
 * {
   box-sizing: border-box;
 }
-
 html {
   font-family: sans-serif;
 }
-
 body {
   margin: 0;
 }
 
-.container {
-  margin: 3rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-  text-align: center;
+main {
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #00006b;
+  background-color: transparent;
+  color: #00006b;
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+}
+button:hover,
+button:active {
+  background-color: #efefff;
+}
+
+button.selected {
+  background-color: #00006b;
+  color: white;
 }
 </style>
